b46aba89ec970ded9b5cc26f81fc7aff6d16f9af
Joe Osowski <joe.osowski@gmail.com>
2014-05-23 14:01:00 -0700

    License updates

--- webapp/src/main/java/org/ambraproject/action/article/BrowseIssueAction.java
+++ webapp/src/main/java/org/ambraproject/action/article/BrowseIssueAction.java
@@ -1,7 +1,6 @@
-/* $HeadURL$
- * $Id$
+/*
+ * Copyright (c) 2007-2014 by Public Library of Science
  *
- * Copyright (c) 2006-2010 by Public Library of Science
  * http://plos.org
  * http://ambraproject.org
  *
@@ -9,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
